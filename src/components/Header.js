677087607a21c2aa601e546b692cadc91ad69ceb
Bashir-Kalyesubula <bashirkalyesubula2@gmail.com>
2023-09-04 00:41:20 -0500

    second commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+
+function Header (){
+return (
+<>
+<header>
+<nav>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Online Order</li>
+    </ul>
+</nav>
+<img></img>
+<img></img>
+
+</header></>
+) }
+
+export default Header;
